00188da11e6c08ce7ec8a7e6d67d659642a8ff15
Ghesa <124006021+KaioGhesa@users.noreply.github.com>
2024-06-12 17:53:36 -0300

    Add files via upload

---
+++ Ref_Out/Ref_Out/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Ref_Out
+{
+   class Calculator
+    {
+        /** Referencia de Ref
+        public static void Triple(ref int x)
+        {
+            x = x * 3;
+        }
+        **/
+
+        public static void Triple(int orign, out int result)
+        {
+            result = orign * 3;
+        }
+    }
+}
---
+++ Ref_Out/Ref_Out/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ref_Out
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * 
+             *Modificador Ref 
+             *
+            int a = 10;
+
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+            **/
+
+            int a = 10;
+            int triple;
+
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
